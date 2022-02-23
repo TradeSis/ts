3627af0f77e541a238d85c95de90f155090badf0
Helio Tradesis <tradesis@tradesis.com.br>
2022-02-23 17:46:22 -0300

    retirando pasta exemplos

--- exemplos/webix-crud/footer.js
+++
@@ -1,7 +0,0 @@
-
-const footer = {
-    height: 30,
-    template:"The software is provided by <a href='http://tradesis.com.br/'>tradesis.com.br</a>. All rights reserved (c)",
-    css:"bottom-link"
-}
-
